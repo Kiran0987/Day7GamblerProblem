b5ba1a2e17d171e27012a72f3e6b455810b29b59
kiran <sharmakiran237@gmail.com>
2022-05-27 12:45:31 +0530

    win or loss added

--- Gambler.java
+++ Gambler.java
@@ -8,11 +8,21 @@ public class Gambler{
 		System.out.println("Stake is:" + stake);
 		System.out.println("Bet price:" + betPrice);
 	}
+	public static boolean winOrLoss() {
+		if(Math.random()>0.5)
+			return true;
+		else 
+			return false;
+	}
 
 	public static void main(String[] args) {
 
 		System.out.println("Gambler simulation program");
+		System.out.println(winOrLoss());
 		Gambler gambler = new Gambler();
 		gambler.display();
+		winOrLoss();
+		
+		
 	}
 }
